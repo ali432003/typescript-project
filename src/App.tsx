04386fa40a1b3961ca2b18d08ff008d6263000d3
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-03-27 04:20:09 -0700

    added react-toastitfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,64 @@
-import { useState } from 'react'
+import React from 'react';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import "react-toastify/dist/ReactToastify.css";
+import { Slide, ToastContainer } from "react-toastify";
+import { ToastAlert } from "./utils/toast"
 
+
+interface dataType {
+  readonly id : number
+  thumbnail : string
+  title : string
+  price : number
+  description : string
+  images : string[]
+  rating : number
+}
 
 function App() {
-  
 
-  
-  
-  
-  // let count : number
-  let [count,setCount] = useState<string>("")
-  
+  const [data, setData] = useState<dataType[]>([])
+
+  const fetchApi = () => {
+    axios.get("https://dummyjson.com/products?limit=10")
+      .then((res) => {
+        console.log(res.data.products);
+        setData(res.data.products)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+  useEffect(() => {
+    fetchApi()
+  }, []);
 
   return (
     <>
-      <h1 className="text-xl " onClick={()=>{setCount("haha heheh")}}>
-        Hello world! {count}
-      </h1>
+      <div className="text-xl flex flex-wrap justify-center gap-2">
+        
+        hello world!
+        
+      </div>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Slide}
+      />
     </>
+
   )
 }
 
